7824e344692cd02af2e287d9665a614eec0e7647
Michael Barkov <geek@bk.ru>
2022-01-15 18:42:51 +0300

    fix https://www.shellcheck.net/

--- 12-proc/psax.sh
+++ 12-proc/psax.sh
@@ -3,31 +3,31 @@
 fmt="%-10s%-10s%-60s%-5s%-10s\n"
 printf "$fmt" USER PID STAT RSS COMMAND
 
-for proc in `ls /proc/ | egrep "^[0-9]" | sort -n`
+for proc in $(find /proc -maxdepth 1 -type d -printf "%f\n" | grep -E "^[0-9]" | sort -n)
 do
 
     if [[ -f /proc/$proc/status ]]
         then
         PID=$proc
 
-    COMMAND=`cat /proc/$proc/cmdline | strings -1`
+    COMMAND=$(cat /proc/"$proc"/cmdline | strings -1)
     if [ "$COMMAND" != '' ]
         then
-        COMMAND="[`awk '/Name/{print $2}' /proc/$proc/status`]"
+        COMMAND="[$(awk '/Name/{print $2}' /proc/"$proc"/status)]"
     else
-        COCOMMANDMM=`cat /proc/$proc/cmdline`
+        COMMAND=$(cat /proc/"$proc"/cmdline)
     fi
 
-    User=`awk '/Uid/{print $2}' /proc/$proc/status`
-    Stat=`cat /proc/$proc/status | awk '/State/{print $2}'`
-    RSS=`cat /proc/$proc/status | awk '/VmRSS/{print $2}'`
+    User=$(awk '/Uid/{print $2}' /proc/"$proc"/status)
+    Stat=$(cat /proc/"$proc"/status | awk '/State/{print $2}')
+    RSS=$(cat /proc/"$proc"/status | awk '/VmRSS/{print $2}')
     if [[ User -eq 0 ]]
        then
        UserName='root'
     else
-       UserName=`grep $User /etc/passwd | awk -F ":" '{print $1}'`
+       UserName=$(grep "$User" /etc/passwd | awk -F ":" '{print $1}')
     fi
-    printf "$fmt" $UserName $PID $Stat $RSS "$COMMAND"
+    printf "$fmt" "$UserName" "$PID" "$Stat" "$RSS" "$COMMAND"
     fi
 done
 
