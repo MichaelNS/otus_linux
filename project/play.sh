b30e6617910e6657b9625502e0d489f9b9d680a5
Michael Barkov <geek@bk.ru>
2022-06-06 00:10:06 +0300

    save

--- project/play.sh
+++ project/play.sh
@@ -7,3 +7,4 @@ ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ans
 
 # ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t routers
 # ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t web
+# ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t nginx
