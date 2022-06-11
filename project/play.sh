31c89f62774043b09a955196f813c05c5150c3bc
Michael Barkov <geek@bk.ru>
2022-06-11 13:50:21 +0300

    project v.0.1

---
+++ project/play.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
+
+# tags
+# ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --list-tags
+
+# ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t routers
+# ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t web
+# ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t nginx
+
+# ansible-playbook site.yml -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory -t routers,web --skip-tags skip_ansible_lint
